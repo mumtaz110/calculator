c94743a542f60ae012d5f48e6cc36b28fe1d4efd
mumtaz110 <159236585+mumtaz110@users.noreply.github.com>
2024-03-23 10:29:11 +0500

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,28 @@
+//  #! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication",
+            "Division",],
+    }
+]);
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("invalid input");
+}
